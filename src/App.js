8ef27a31d8058fca630dfed1feb99369880da259
Ovo Rust <ovojogarust30@gmail.com>
2026-04-02 21:01:11 -0300

    FIX/react version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://futstream-server.onrender.com/api";
 
 const SPINNER_FRAMES = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
